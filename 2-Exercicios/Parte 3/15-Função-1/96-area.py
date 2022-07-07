eecfa3620a2bb9e1a2c33b5277ee2781abce25bf
Alex Sander Luan Marques <alex.marques@sebsa.com.br>
2022-07-07 11:23:33 -0300

    Area m²

---
+++ 2-Exercicios/Parte 3/15-Função-1/96-area.py
@@ -0,0 +1,25 @@
+'''
+Faça um programa que tenha uma função chamada área(),
+que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno.
+'''
+
+
+def titulo():
+    print('Controle de terreno!!!')
+
+
+def linha():
+    print('-'*30)
+
+
+def area(a, b):
+    r = a * b
+    print(f'A área de um terreno {l}x{c} é de {r}m².')
+
+
+titulo()
+linha()
+
+l = float(input('Largura (m): '))
+c = float(input('Comprimento(m): '))
+area(l, c)
